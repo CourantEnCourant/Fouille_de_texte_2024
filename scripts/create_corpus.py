f6ad6e496366519dac7e34bf40fa57cde0f7b9f7
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2024-05-04 20:22:51 +0200

    feat: make create_corpus.py more robust

--- scripts/create_corpus.py
+++ scripts/create_corpus.py
@@ -2,6 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pathlib import Path
 from typing import List
+import re
+
+
+def clean_poem(raw_poem : str) -> str:
+    # Clean a poem
+    note_symbols = r'[\[\]0-9]' # These are notes, like [1]...
+    poem_cleaned = re.sub(note_symbols, '', raw_poem.lower()) # Clean notes
+    poem_cleaned = ' '.join(poem_cleaned.split()) # One poem per line
+
+    return poem_cleaned
+
 
 def get_files(input_folder: Path) -> List[Path]:
     # Recursion func that takes in a folder of corpus, returns paths of all html files
@@ -21,14 +32,14 @@ def get_poems(driver, file_html) -> List[str]:
     # Get poems from a wikisource html file
     clean_poems = []
     driver.get(f'file://{file_html}')
-    raw_poems = driver.find_elements(By.XPATH, '//div[@class="poem"]')
+    raw_poems = driver.find_elements(By.XPATH, '//div[@class="poem" or @class="poem1"]') # "poem1" is for La Fontaine
     print(f'Number of poem in {file_html}: {len(raw_poems)}.') # To verify if this html file is usable.
+    raw_poems = raw_poems[:30] # For each html file, only 30 poems.
 
-    for poem in raw_poems:
-        poem = poem.text.lower() # lower
-        poem_lines = poem.split('\n') # each line a str
-        poem_clean = ' '.join(poem_lines) # remove title in the first line
-        clean_poems.append(poem_clean)
+    for raw_poem in raw_poems:
+        raw_poem = raw_poem.text
+        poem_cleaned = clean_poem(raw_poem)
+        clean_poems.append(poem_cleaned)
 
     return clean_poems
 
@@ -42,7 +53,7 @@ def main(input_folder : Path, period : str):
 
     for html in html_paths:
         poems = get_poems(driver, html)
-        for poem in poems[:30]: # For each html file, only 30 poems.
+        for poem in poems:
             with open(f'../corpus/clean/{period}/poem_{count_file}.txt', 'w', encoding='utf-8') as poem_file:
                 poem_file.write(poem)
                 count_file += 1
@@ -53,7 +64,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Create corpus of a specific period.')
     parser.add_argument('input_folder', type=Path, help='Folder that contains html files of a specific period.')
-    parser.add_argument('period', choices=['renaissance', 'classicism'], help='The period you want to choose.')
+    parser.add_argument('period', choices=['renaissance', 'classicisme', 'romantisme'], help='The period you want to choose.')
 
     args = parser.parse_args()
 
